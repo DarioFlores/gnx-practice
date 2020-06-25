a725ba8c6c6da637a85aed3b9a386dc74fcc2b18
DarioFlores <darioexequiel22@gmail.com>
2020-06-25 09:51:30 -0300

    Iniciando proyecto

---
+++ models/employee.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const employeeFields = {
+    dni: String,
+    birth_date: Date,
+    firsta_name: String,
+    last_name: String,
+    gender: String,
+    hire_date: Date,
+};
+//dni, birth_date, firsta_name, last_name, gender, hire_date
+const employeeSchema = new Schema(employeeFields);
+
+const Employee = mongoose.model("Employee", employeeSchema, "employee");
+Employee.createCollection();
+
+module.exports = { employeeFields, Employee };
