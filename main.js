ce9133f167975b1130ab52ea464205ebb9881df2
DarioFlores <darioexequiel22@gmail.com>
2020-06-25 10:07:35 -0300

    Identificacion de SO

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ const express = require('express');
 const gnx = require('@simtlix/gnx');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+var os = require("os");
 
 const app = express();
+let ipName
+if (os.type() === 'Linux') {
+  ipName = 'localhost'
+} else {
+  ipName = os.hostname()
+}
+console.log(ipName);
 
-mongoose.connect('mongodb://localhost:27017,localhost:27018,localhost:27019/practice', { 
+mongoose.connect(`mongodb://${ipName}:27017,${ipName}:27018,${ipName}:27019/practice`, { 
     replicaSet: 'rs',
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
+
 
 mongoose.connection.once('open', () => {
   console.log('connected to database')
