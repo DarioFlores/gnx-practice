4d2fa5afa9c9fbb1180f6e18210aab85d7eaa0fa
DarioFlores <darioexequiel22@gmail.com>
2020-06-29 17:10:06 -0300

    Conexion a base de datos de pruebas

--- test/app.test.js
+++ test/app.test.js
@@ -1,28 +1,50 @@
-const assert = require('assert');
-const url = 'http://localhost:3000'
-const request = require('supertest')(url);
-const app = require('../app');
+const assert = require("assert");
+const url = "http://localhost:3000";
+const request = require("supertest")(url);
+const app = require("../app");
 
-
-describe('Test de ejemplo', () => {
-    
-    it('Retorna un array Query employess', (done) => {
-        request.get('/graphql')
-        .send({ query: `{
+describe("Test de ejemplo", () => {
+    it("Retorna un array Query employess", (done) => {
+        request
+            .get("/graphql")
+            .send({
+                query: `{
             employess {
               id
               dni
             }
           }
-          `})
-        .expect(200)
-        .end((err,res) => {
-            // res will contain array with one user
-            if (err) return done(err);
-            assert.equal(Array.isArray(res.body.data.employess),true)
-            done();
-        })
-    })
+          `,
+            })
+            .expect(200)
+            .end((err, res) => {
+                // res will contain array with one user
+                if (err) return done(err);
+                assert.equal(Array.isArray(res.body.data.employess), true);
+                done();
+            });
+    });
 
-})
-
+    it("Mutation departament", (done) => {
+        const name = Math.random();
+        request
+            .post("/graphql")
+            .send({ query: `mutation{
+                adddepartment(input: {
+                    dept_name:"${name}"
+                }){
+                    id
+                    dept_name
+                }
+            }`})
+            .expect(200)
+            .end((err, res) => {
+                // res will contain array with one user
+                if (err) return done(err);
+                const data = res.body.data.adddepartment;
+                assert.equal(data.hasOwnProperty("id"), true);
+                assert.equal(data.hasOwnProperty("dept_name"), true);
+                done();
+            });
+    });
+});
