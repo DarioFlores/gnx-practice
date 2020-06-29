ebd23fa0da840d3b8a5f841d45b969be8f26b15d
DarioFlores <darioexequiel22@gmail.com>
2020-06-29 10:40:05 -0300

    Entorno de prueba listo

---
+++ test/test_helper.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const os = require("os");
+
+before( done => {
+    let ipName;
+    if (os.type() === "Linux") {
+        ipName = "localhost";
+    } else {
+        ipName = os.hostname();
+    }
+    let urlDB;
+    let opDB;
+    urlDB = `mongodb://${ipName}:27017,${ipName}:27018,${ipName}:27019/practice_test`;
+    opDB = {
+        replicaSet: "rs",
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    };
+
+    mongoose.connect(urlDB, {...opDB, useCreateIndex: true});
+    mongoose.connection.once("open", () => {
+        console.log("connected to database test");
+    });
+    done()
+})
+
+
+
