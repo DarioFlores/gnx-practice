134ea6806380156b781ed5c9ad4fd12887880456
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-07-01 10:44:33 -0300

    Agreando test mutation employee

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const url = "http://localhost:3000";
 const request = require("supertest")(url);
 const app = require("../app");
+const { isObject } = require("util");
 
 
 describe("Test de ejemplo", function (){
@@ -78,5 +79,93 @@ describe("Test de ejemplo", function (){
                 assert.equal(data.extensions.code, 'dept_name is already exist');
                 assert.equal(data.extensions.status, 'Can not create a department with same name');
             });
+    });
+
+    it("Probando Query department retorna un array",function(done){
+        request 
+        .get("/graphql")
+        .send({query:`{
+            departments{
+                id
+                dept_name
+            }}
+        `})
+        .expect(200)
+        .end((err,res)=> {
+            if(err) return done(err);
+            assert.equal(Array.isArray(res.body.data.departments),true);
+            done();
+        });
+    });
+
+    it("Probando Mutation employee", function(done){
+        const dni = Math.random()
+        request
+        .post("/graphql")
+        .send({query:`
+            mutation{
+                addemployee(input:{
+                    dni:"${dni}"
+                }){
+                id
+                dni
+                first_name
+                last_name
+                gender
+                birth_date
+                hire_date
+                }
+            }
+        `})
+        .expect(200)
+        .end((err,res)=> {
+            if(err) return done(err);
+            const data = res.body.data.addemployee
+            console.log(data);
+            assert.equal(data.hasOwnProperty("id"),true);
+            assert.equal(data.hasOwnProperty("dni"),true);
+            assert.equal(data.hasOwnProperty("first_name"),true);
+            assert.equal(data.hasOwnProperty("last_name"),true);
+            assert.equal(data.hasOwnProperty("gender"),true);
+            assert.equal(data.hasOwnProperty("birth_date"),true);
+            assert.equal(data.hasOwnProperty("hire_date"),true);
+            done()
+        });
+    });
+
+    it("Query que devuelve un solo empleado por medio de su id",async function(){
+        let dni = Math.random();
+        let Employee = require('../models/employee').Employee;
+        const testEmployee = await Employee.create({dni});
+        //console.log('MIRA ESTA LINEAAAAAAAAAAAAAA',testEmployee);
+        request
+        .get("/graphql")
+        .send({query: `
+            {
+                employee(id:"${testEmployee._id}"){
+                    id
+                    dni
+                    first_name
+                    last_name
+                    gender
+                    birth_date
+                    hire_date
+                }
+            }
+        `})
+        .expect(200)
+        .end((err,res)=> {
+            const data= res.body.data.employee
+            //console.log('MIRA LA DATAAAAAAAAAAAAAAA',data);
+            if(err) return done(err);
+            assert.equal(isObject(res.body.data.employee),true);
+            assert.equal(data.hasOwnProperty("id"),true);
+            assert.equal(data.hasOwnProperty("dni"),true);
+            assert.equal(data.hasOwnProperty("first_name"),true);
+            assert.equal(data.hasOwnProperty("last_name"),true);
+            assert.equal(data.hasOwnProperty("gender"),true);
+            assert.equal(data.hasOwnProperty("birth_date"),true);
+            assert.equal(data.hasOwnProperty("hire_date"),true);
+        })
     });
 });
